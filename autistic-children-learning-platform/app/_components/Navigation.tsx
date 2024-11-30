941fb0c1c2aa214c49dac297bcced2f4cfb1674a
Hassan <htarek1011@gmail.com>
2024-11-30 14:40:05 +0200

    refactor: update layout and add header component; configure Tailwind CSS colors

---
+++ autistic-children-learning-platform/app/_components/Navigation.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { auth } from "../_lib/auth";
+export default async function Navigation() {
+  // const session = await auth();
+
+  return (
+    <nav className="z-10 text-xl">
+      <ul className="flex gap-16 items-center">
+        <li>
+          <Link href="/" className="hover:text-accent-400 transition-colors">
+            Games
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="hover:text-accent-400 transition-colors">
+            Chat
+          </Link>
+        </li>
+        <li>
+          {/* {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center gap-4"
+            >
+              <img
+                className="h-8 rounded-full"
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy="no-referrer"
+              />
+              {session.user.name}
+            </Link>
+          ) : ( */}
+          <Link
+            href="/account"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Guest area
+          </Link>
+          {/* )} */}
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ autistic-children-learning-platform/app/_components/SignInButton.tsx
@@ -0,0 +1,22 @@
+//we cant use onClick -> instead use server action to keep it server component
+
+//Todo change
+import { signInAction } from "../_actions/signInAction";
+
+function SignInButton() {
+  return (
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
+  );
+}
+
+export default SignInButton;
