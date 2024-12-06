c4e6b941335f3aadd7763ee0cfeef62ec9d422f1
Hassan <htarek1011@gmail.com>
2024-12-06 15:26:58 +0200

    feat: add Godot game integration; create page and include game assets for autism learning platform

---
+++ autistic-children-learning-platform/app/guess_emotion/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl text-accent-500">My Godot Game</h1>
+      <div className="flex justify-center mt-10">
+        <iframe
+          src="/godot-game/Autism.html"
+          width="1000"
+          height="600"
+          style={{
+            border: "none",
+            maxWidth: "100%",
+            height: "",
+          }}
+          title="Godot Game"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
