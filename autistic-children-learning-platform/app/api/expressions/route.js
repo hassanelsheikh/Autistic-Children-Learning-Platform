1167790970759f6424a22c2f2d6ecad48b375a57
asmaahamza1 <asmaahaamza@gmail.com>
2024-12-08 01:49:42 +0200

    feedback

--- autistic-children-learning-platform/app/api/expressions/route.js
+++ autistic-children-learning-platform/app/api/expressions/route.js
@@ -14,5 +14,9 @@ export default async function handler(req, res) {
       res.setHeader('Allow', ['POST']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
     }
+    }
+    
+
+
 
 
