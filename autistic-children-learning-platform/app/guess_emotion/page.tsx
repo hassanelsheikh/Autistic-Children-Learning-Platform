9041d5024d5e641471b74727e10f9dae1558ef49
Hassan <htarek1011@gmail.com>
2024-12-07 12:53:06 +0200

    feat: update game title and configuration; change header to "Guess How I Feel!" and adjust file size in Godot config

--- autistic-children-learning-platform/app/guess_emotion/page.tsx
+++ autistic-children-learning-platform/app/guess_emotion/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1 className="text-center text-2xl text-accent-500">Guess my Emotion</h1>
+      {/* <h1 className="text-center text-5xl text-accent-500">Guess How I Feel!</h1> */}
       <div className="flex justify-center mt-10">
         <iframe
           src="/godot-game/Autism.html"
