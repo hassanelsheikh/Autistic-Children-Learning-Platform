8aa74e937e79a69d8bb54b118c49441a653ed4ec
Hassan <htarek1011@gmail.com>
2024-12-07 23:14:25 +0200

    feat: add rating component and authentication to emotion guessing game; implement user session handling and integrate MUI for rating

--- autistic-children-learning-platform/app/guess_emotion/page.tsx
+++ autistic-children-learning-platform/app/guess_emotion/page.tsx
@@ -1,4 +1,8 @@
-export default function Page() {
+import { auth } from "@app/_lib/auth";
+import Rate from "@app/_components/Rate";
+
+export default async function Page() {
+  const session = await auth();
   return (
     <div>
       {/* <h1 className="text-center text-5xl text-accent-500">Guess How I Feel!</h1> */}
@@ -15,6 +19,17 @@ export default function Page() {
           title="Godot Game"
         ></iframe>
       </div>
+
+      {session ? (
+        <div>
+          <h2 className="text-2xl text-accent-500 text-center mt-10">
+            Rate the game
+          </h2>
+          <div className="flex justify-center">
+            <Rate />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
