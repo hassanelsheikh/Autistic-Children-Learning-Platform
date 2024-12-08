0cbd3f7c951303cd69ab80b33af15adda806bde9
asmaahamza1 <asmaahaamza@gmail.com>
2024-12-08 23:18:43 +0200

    get top expressions

--- autistic-children-learning-platform/app/api/expressions/route.js
+++ autistic-children-learning-platform/app/api/expressions/route.js
@@ -1,5 +1,7 @@
 //function to save expression
 import { saveExpression } from "../../_lib/data_service";
+import { calculateTopExpressions } from "../../_lib/data_service";
+
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
@@ -15,8 +17,22 @@ export default async function handler(req, res) {
       res.status(405).end(`Method ${req.method} Not Allowed`);
     }
     }
-    
 
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+      const { email } = req.body;
 
+        try {
+      await calculateTopExpressions(email);
+      res.status(200).json({ message: "Top expressions calculated and saved." });
+    } catch (error) {
+      console.error("Error calculating top expressions:", error);
+      res.status(500).json({ error: "Error calculating top expressions." });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
 
 
