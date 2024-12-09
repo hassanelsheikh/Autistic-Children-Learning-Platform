6fe6707f3dcb570ab686555c69c663908d906b45
Hassan <htarek1011@gmail.com>
2024-12-09 16:29:52 +0200

    feat: wrap game images in links for improved navigation

--- autistic-children-learning-platform/app/games/page.tsx
+++ autistic-children-learning-platform/app/games/page.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Games() {
   return (
     <div className="mt-10 px-4">
       <h1 className="text-4xl font-bold text-center mb-8">Games</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-
         <div className="rounded-lg overflow-hidden shadow-lg bg-lightblue transition-transform transform hover:scale-105 duration-300">
-          <div className="relative w-full h-96">
-            <Image
-              src="/everyday-activities.jpg"
-              alt="Game 1"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-t-lg"
-            />
-          </div>
+          <Link href="/games/game1">
+            <div className="relative w-full h-96">
+              <Image
+                src="/everyday-activities.jpg"
+                alt="Game 1"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-t-lg"
+              />
+            </div>
+          </Link>
           <div className="p-4">
             <h2 className="text-2xl font-semibold mb-2">Everyday Activities</h2>
             <p className="text-darkBlue">
@@ -25,15 +27,17 @@ export default function Games() {
         </div>
 
         <div className="rounded-lg overflow-hidden shadow-lg bg-lightblue transition-transform transform hover:scale-105 duration-300">
-          <div className="relative w-full h-96">
-            <Image
-              src="/Guess that feeling.jpg"
-              alt="Game 2"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-t-lg"
-            />
-          </div>
+          <Link href="/games/game2">
+            <div className="relative w-full h-96">
+              <Image
+                src="/Guess that feeling.jpg"
+                alt="Game 2"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-t-lg"
+              />
+            </div>
+          </Link>
           <div className="p-4">
             <h2 className="text-2xl font-semibold mb-2">Guess that Feeling</h2>
             <p className="text-darkBlue">
